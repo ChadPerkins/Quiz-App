71a64e3f7441625df1989cae07b36841d7bfb62d
ChadPerkins <web3.engineering@gmail.com>
2022-04-09 16:05:37 -0400

    created react front end and started working on login form

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { CssBaseline, createTheme, ThemeProvider } from "@mui/material";
+
+const theme = createTheme({
+    palette: {
+        mode: "dark",
+    },
+});
+
+ReactDOM.render(
+    <React.StrictMode>
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <App />
+        </ThemeProvider>
+    </React.StrictMode>,
+    document.getElementById("root")
+);
