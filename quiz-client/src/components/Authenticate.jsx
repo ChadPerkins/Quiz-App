33cc575903aa40a76ac2131dd9c1c2880f19a663
ChadPerkins <web3.engineering@gmail.com>
2022-04-13 15:57:25 -0400

    added screenshots for readme

--- quiz-client/src/components/Authenticate.jsx
+++ quiz-client/src/components/Authenticate.jsx
@@ -4,6 +4,5 @@ import useStateContext from "../hooks/useStateContext";
 
 export default function Authenticate() {
     const { context } = useStateContext();
-
     return context.participantId == 0 ? <Navigate to="/" /> : <Outlet />;
-}
+} 
